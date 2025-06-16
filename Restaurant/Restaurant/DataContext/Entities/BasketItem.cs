48990e5cfeff26af844658f3625dbf3c12e6f26b
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2025-06-16 15:09:20 +0400

    sabiV2

---
+++ Restaurant/Restaurant/DataContext/Entities/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurant.DataContext.Entities
+{
+    public class BasketItem
+    {
+        public int MenuItemId { get; set; }
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Restaurant/Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
+++ Restaurant/Restaurant/obj/Debug/net8.0/Restaurant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7a41891331721250212405c1f1a16dbc440b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+189272310b72be788b7e3e71cfd18588f76288dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
